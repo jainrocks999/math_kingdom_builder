b41b8be2dcb1a1cb850dbe2a59b466fb6c5b2af8
Raju Barde <rajubarde587@gmail.com>
2026-06-19 11:20:40 +0530

    okay

--- android/build/ios/SourcePackages/artifacts/rive_native-0.1.7/RiveNative/RiveNative_ios.xcframework/ios-arm64/Headers/rive_native/external_objc.h
+++
@@ -1,9 +0,0 @@
-#ifndef _SBS_EXTERNAL_OBJC_H_
-#define _SBS_EXTERNAL_OBJC_H_
-
-#include <cstdint>
-#import <Metal/Metal.h>
-
-void preCommitCallback(id<MTLCommandBuffer>, void*, void*, void*);
-
-#endif
